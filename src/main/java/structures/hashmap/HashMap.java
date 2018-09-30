b67c22c4b1945dd3000b9c90297c5df931eee453
Kubset <kuba1228334@gmail.com>
2018-09-30 16:52:16 +0200

    add skeleton of Hashmap class, add skeleton methods to implement

---
+++ src/main/java/structures/hashmap/HashMap.java
@@ -0,0 +1,78 @@
+package structures.hashmap;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMap<K,V> implements Map {
+    int bucketSize;
+    float loadFactor;
+
+
+    public HashMap() {
+    }
+
+    public HashMap(int bucketSize) {
+        this.bucketSize = bucketSize;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
+    }
+}
