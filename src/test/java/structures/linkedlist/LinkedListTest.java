8f382c2052577cb9f4c838fce198f918db2b4a62
Kubset <kuba1228334@gmail.com>
2018-10-01 22:42:34 +0200

    add test addfewElements to linkedlist

--- src/test/java/structures/linkedlist/LinkedListTest.java
+++ src/test/java/structures/linkedlist/LinkedListTest.java
@@ -1,7 +1,27 @@
 package structures.linkedlist;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
+
+    @Test
+    public void test_addFewElementsToLinkedList() {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(22);
+        ll.add(2);
+        ll.add(1);
+        ll.add(32);
+        ll.add(22);
+        assertAll(() -> {
+            assertEquals(22, ll.get(0));
+            assertEquals(2, ll.get(1));
+            assertEquals(1, ll.get(2));
+            assertEquals(32, ll.get(3));
+            assertEquals(22, ll.get(4));
+        });
+
+    }
 
 }
