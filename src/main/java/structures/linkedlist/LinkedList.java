9834741f5eaa9a40e28e010ac2744bf629477d1d
Kubset <kuba1228334@gmail.com>
2018-10-02 20:32:18 +0200

    implement add and get method

--- src/main/java/structures/linkedlist/LinkedList.java
+++ src/main/java/structures/linkedlist/LinkedList.java
@@ -1,4 +1,35 @@
 package structures.linkedlist;
 
-public class LinkedList {
+public class LinkedList<T> {
+
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+    private int size;
+
+
+    public LinkedList() {
+        this.size = 0;
+    }
+
+    public void add(T element) {
+        if(size == 0) {
+            firstNode = lastNode = new Node<>(element);
+        } else {
+            lastNode.setNext(new Node<>(element));
+            lastNode = lastNode.getNext();
+        }
+        size++;
+
+    }
+
+    public T get(int index) {
+        Node<T> currentNode = firstNode;
+        for(int i=0; i<index; i++) {
+            currentNode = currentNode.getNext();
+
+        }
+        return currentNode.getValue();
+    }
+
+
 }
