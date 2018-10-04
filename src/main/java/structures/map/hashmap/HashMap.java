b33ea81adc8b009c1e49dc78631634c2ce9d996c
Kubset <kuba1228334@gmail.com>
2018-10-04 17:08:31 +0200

    change tree structure of maps

---
+++ src/main/java/structures/map/hashmap/HashMap.java
@@ -0,0 +1,186 @@
+package structures.map.hashmap;
+
+//import java.util.*;
+
+import structures.List.linkedlist.LinkedList;
+import structures.map.Map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashMap<K,V> implements Map<K,V> {
+    public int bucketSize;
+    private float loadFactor;
+    private LinkedList<KeyValue<K,V>>[] elements;
+    private int size;
+
+    public HashMap() {
+        size = 0;
+        bucketSize = 16;
+        loadFactor = 0.8F;
+        this.elements = new LinkedList[bucketSize];
+    }
+
+    public HashMap(int bucketSize) {
+        size = 0;
+        this.loadFactor = 0.8F;
+        this.bucketSize = bucketSize;
+        this.elements = new LinkedList[bucketSize];
+    }
+
+    public HashMap(int bucketSize, float loadFactor) {
+        size = 0;
+        this.loadFactor = loadFactor;
+        this.bucketSize = bucketSize;
+        this.elements = new LinkedList[bucketSize];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+
+
+    public boolean containsKey(K key) {
+        boolean isKey = false;
+
+        for(int i=0; i<elements.length; i++) {
+            if(elements[i] != null) {
+                for(KeyValue kv : elements[i]) {
+                    if(kv.getKey().equals(key)) {
+                        isKey = true;
+                    }
+                }
+            }
+        }
+        return isKey;
+    }
+
+    public boolean containsValue(V value) {
+        boolean isValue = false;
+
+        for(int i=0; i<elements.length; i++) {
+            if(elements[i] != null) {
+                for(KeyValue kv : elements[i]) {
+                    if(kv.getValue().equals(value)) {
+                        isValue = true;
+                    }
+                }
+            }
+        }
+        return isValue;
+    }
+
+    public V get(K key) {
+        int position = getHash(key);
+        if (elements[position] == null) return null;
+        else {
+            for(KeyValue<K,V> keyValue: elements[position]) {
+                if(keyValue.getKey().equals(key)) {
+                    return keyValue.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value) {
+        resizeIfNeeded();
+        KeyValue<K,V> keyValue = new KeyValue<K,V>(key, value);
+        boolean isKeyPresent = false;
+        int position = getHash(key);
+        if(elements[position] == null) {
+            elements[position] = new LinkedList<>();
+            elements[position].add(keyValue);
+            size++;
+        } else {
+            for(KeyValue<K,V> kv : elements[position]) {
+                if(kv.getKey().equals(key)) {
+                    kv.setValue(value);
+                    isKeyPresent = true;
+                }
+            }
+            if(!isKeyPresent) {
+                elements[position].add(keyValue);
+                size++;
+            }
+
+        }
+    }
+
+    public void remove(K key) {
+        int position = getHash(key);
+        if(elements[position] != null) {
+            for(int i=0; i<elements[position].size(); i++) {
+                if(elements[position].get(i).getKey().equals(key)) {
+                    elements[position].remove(i);
+                    size--;
+                }
+            }
+        }
+
+
+    }
+
+    public void clear() {
+        elements = new LinkedList[bucketSize];
+        size = 0;
+    }
+
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        for(int i=0; i<elements.length;i++) {
+            if(elements[i] != null) {
+                elements[i].forEach((keyValue) -> keys.add(keyValue.getKey()));
+            }
+        }
+        return keys;
+    }
+
+
+    public Set<V> values() {
+         Set<V> values = new HashSet<>();
+        for(int i=0; i<elements.length;i++) {
+            if(elements[i] != null) {
+                elements[i].forEach((keyValue) -> values.add(keyValue.getValue()));
+            }
+        }
+        return values;
+    }
+
+    private int getHash(K key) {
+        return key.hashCode() % bucketSize;
+    }
+
+    private void resizeIfNeeded() {
+        if(size > bucketSize*loadFactor) {
+            initializeNewBucket();
+            bucketSize *= 2;
+        } else if(size < bucketSize*loadFactor/2) {
+            initializeNewBucket();
+            bucketSize /= 2;
+        }
+
+
+
+    }
+
+    private void initializeNewBucket() {
+        LinkedList<KeyValue<K,V>>[] temp = new LinkedList[bucketSize*2];
+        KeyValue<K,V> keyValue;
+        for(K key : keySet()) {
+            keyValue = new KeyValue<>(key, get(key));
+            int position = getHash(key);
+            if(temp[position] == null) {
+                temp[position] = new LinkedList<>();
+            }
+            temp[position].add(keyValue);
+       }
+       elements = temp;
+    }
+}
---
+++ src/test/java/structures/map/hashmap/HashMapTest.java
@@ -0,0 +1,262 @@
+package structures.map.hashmap;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashMapTest {
+
+    @Test
+    protected void test_addFewElementsToHashMap() {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(11,22);
+        hm.put(10,1);
+        hm.put(2,4);
+        hm.put(3,222);
+
+        assertAll(() -> {
+            assertEquals(new Integer(22), hm.get(11));
+            assertEquals(new Integer(1), hm.get(10));
+            assertEquals(new Integer(4), hm.get(2));
+            assertEquals(new Integer(222), hm.get(3));
+                });
+    }
+
+    @Test
+    protected void test_increaseSize() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+        int size = hm.size();
+        hm.put(10,1);
+        hm.put(71,4);
+        hm.put(83,222);
+        int increasedSize = hm.size();
+
+         assertAll(() -> {
+            assertEquals(6, size);
+            assertEquals(8, increasedSize);
+                });
+    }
+
+    @Test
+    protected void test_reduceSize() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+        int size = hm.size();
+        hm.remove(10);
+        hm.remove(2);
+        int reducedSize = hm.size();
+
+        assertAll(() -> {
+            assertEquals(6, size);
+            assertEquals(4, reducedSize);
+        });
+
+    }
+
+    @Test
+    protected void test_cleanMap() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        hm.clear();
+
+        assertEquals(0, hm.size());
+
+    }
+
+    @Test
+    protected void test_isEmpty() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        boolean notEmpty = hm.isEmpty();
+        hm.clear();
+        boolean empty = hm.isEmpty();
+
+
+        assertAll(() -> {
+            assertTrue(empty);
+            assertFalse(notEmpty);
+        });
+    }
+
+    @Test
+    protected void test_returnKeySet() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        Set<Integer> result = hm.keySet();
+        Set<Integer> expected = new HashSet<>(Arrays.asList(10,2,3,100,0,22));
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    protected void test_returnEmptyKeySet() {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        Set<Integer> emptySet = new HashSet<>();
+
+        assertEquals(emptySet, hm.keySet());
+    }
+
+    @Test
+    protected void test_returnValues() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        Set<Integer> result = hm.values();
+        Set<Integer> expected = new HashSet<>(Arrays.asList(1,4,222,12,422,2222));
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    protected void test_returnEmptyValues() {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        Set<Integer> emptySet = new HashSet<>();
+
+        assertEquals(emptySet, hm.values());
+    }
+
+    @Test
+    protected void test_containsKey() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        boolean notPresentKey = hm.containsKey(1234567);
+        boolean presentKey = hm.containsKey(10);
+
+        assertAll(() -> {
+            assertTrue(presentKey);
+            assertFalse(notPresentKey);
+        });
+
+    }
+
+    @Test
+    protected void containsValue() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+
+        boolean notPresentValue = hm.containsValue(1234567);
+        boolean presentValue = hm.containsValue(222);
+
+        assertAll(() -> {
+            assertTrue(presentValue);
+            assertFalse(notPresentValue);
+        });
+
+    }
+
+    @Test
+    protected void test_putToPresentKey() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+        int size = hm.size();
+
+        hm.put(10,222);
+
+
+        assertAll(() -> {
+            assertEquals(size, hm.size());
+            assertEquals(new Integer(222), hm.get(10));
+        });
+
+    }
+
+    @Test
+    protected void test_removeAbsentKey() {
+        HashMap<Integer, Integer> hm = produceFilledHashMap();
+        int size = hm.size();
+
+        hm.remove(9999);
+
+        assertEquals(size, hm.size());
+    }
+
+    @Test
+    protected void test_increaseBucketSize() throws IllegalAccessException, NoSuchFieldException {
+        HashMap<Integer, Integer> hm = produceFilledHashMap(1000);
+        Field f = hm.getClass().getDeclaredField("bucketSize");
+        f.setAccessible(true);
+        int bucketSize = (int) f.get(hm);
+
+        assertEquals(2048, bucketSize);
+    }
+
+    @Test
+    protected void test_reduceBucketSize() throws IllegalAccessException, NoSuchFieldException {
+        HashMap<Integer, Integer> hm = produceFilledHashMap(2);
+        Field f = hm.getClass().getDeclaredField("bucketSize");
+        f.setAccessible(true);
+        int bucketSize = (int) f.get(hm);
+
+        assertEquals(4, bucketSize);
+    }
+
+    @Test
+    protected void test_increaseBucketSizeWithCustomLoadFactor() throws IllegalAccessException, NoSuchFieldException {
+        HashMap<Integer, Integer> hm = produceFilledHashMap(250, 0.5F);
+        Field f = hm.getClass().getDeclaredField("bucketSize");
+        f.setAccessible(true);
+        int bucketSize = (int) f.get(hm);
+
+        assertEquals(512, bucketSize);
+    }
+
+    @Test
+    protected void test_reduceBucketSizeWithCustomLoadFactor() throws IllegalAccessException, NoSuchFieldException {
+        HashMap<Integer, Integer> hm = produceFilledHashMap(4, 1.1F);
+        Field f = hm.getClass().getDeclaredField("bucketSize");
+        f.setAccessible(true);
+        int bucketSize = (int) f.get(hm);
+
+        assertEquals(4, bucketSize);
+    }
+
+
+    private HashMap<Integer, Integer> produceFilledHashMap() {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(10,1);
+        hm.put(2,4);
+        hm.put(3,222);
+        hm.put(100,12);
+        hm.put(0,422);
+        hm.put(22,2222);
+        return hm;
+    }
+
+    private HashMap<Integer, Integer> produceFilledHashMap(int size) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        List<Integer> keys = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        initialize(keys, size);
+        initialize(values, size);
+
+        for(int i=0 ;i<size; i++) {
+            hm.put(keys.get(i), values.get(i));
+        }
+
+        return hm;
+    }
+
+    private HashMap<Integer, Integer> produceFilledHashMap(int size, float loadFactor) {
+        HashMap<Integer, Integer> hm = new HashMap<>(16,loadFactor);
+        List<Integer> keys = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+        initialize(keys, size);
+        initialize(values, size);
+
+        for(int i=0 ;i<size; i++) {
+            hm.put(keys.get(i), values.get(i));
+        }
+
+        return hm;
+    }
+
+
+    public static void initialize(List<Integer> list, int size) {
+       Random rand = new Random();
+       for(int i=0; i<size; i++) {
+           list.add(rand.nextInt(10000000));
+       }
+    }
+
+}
